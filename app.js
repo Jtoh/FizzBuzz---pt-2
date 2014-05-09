cfb18d799746e9f726c7174d49339dcdda376414
jonathanoh0689 <jonathanoh0689@gmail.com>
2014-05-08 20:24:38 -0700

    Initial Sync

---
+++ app.js
@@ -0,0 +1,23 @@
+
+// $(document).ready(function() {
+
+
+// for (i=0; i<100; i++) {
+// 	if (i%3===0 && i%5===0) {
+// 		console.log("Fizzbuzz");
+// 	}
+// 	else if(i%3===0) {
+// 		console.log("Fizz");
+// 	}
+// 	else if(i%5===0) {
+// 		console.log("Buzz");
+// 	}
+// 	console.log(i);
+// }
+// });
+
+$(document).ready(function() {
+var prompt = prompt("Enter a number", "0");
+var number = parseInt(prompt);
+
+});
